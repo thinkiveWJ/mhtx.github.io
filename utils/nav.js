7cc4e4b72cf9ec21509fc527b0100768fdf3ec5d
love <wangjunzmd@163.com>
2021-05-17 12:33:34 +0800

    移动端惠天下

---
+++ utils/nav.js
@@ -0,0 +1,18 @@
+$(function(){
+  $(".nav-bar").click(function(e){
+    e.preventDefault();
+    $(".nav-list").fadeToggle();
+  });
+
+  // dialog
+  $(".dialog-bg, .dialog-close").click(function(){
+    $(".dialog-bg").fadeOut();
+    $(".dialog").fadeOut();
+  });
+  $(".fixed-bottom ul li:last-child a").click(function(e){
+    e.preventDefault();
+    $(".dialog-bg, .dialog").fadeIn();
+  });
+
+
+});
